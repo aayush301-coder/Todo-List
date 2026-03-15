a627e0c7c67cbbd6393a473722a01ae8df18e1bd
aayush301-coder <aayushgarg301@gmail.com>
2026-03-15 19:31:10 +0530

    Added better js

--- todo.js
+++ todo.js
@@ -1,38 +1,71 @@
+const todoInput = document.getElementById("todo-input");
+const todoDate = document.getElementById("todo-date");
+const addBtn = document.getElementById("add-btn");
+const todoContainer = document.getElementById("todo-container");
+
 let todoList = [
   {
-    item: 'Buy Milk',
-    dueDate: '4/10/2023'
+    item: "Buy Milk",
+    dueDate: "2023-10-04"
   },
   {
-    item: 'Go to College',
-    dueDate: '4/10/2023'
+    item: "Go to College",
+    dueDate: "2023-10-04"
   }
 ];
 
-displayItems();
+addBtn.addEventListener("click", addTodo);
 
 function addTodo() {
-  let inputElement = document.querySelector('#todo-input');
-  let dateElement = document.querySelector('#todo-date');
-  let todoItem = inputElement.value;
-  let todoDate = dateElement.value;
-  todoList.push({item: todoItem, dueDate: todoDate});
-  inputElement.value = '';
-  dateElement.value = '';
-  displayItems();
+
+  const item = todoInput.value.trim();
+  const date = todoDate.value;
+
+  if (item === "" || date === "") {
+    alert("Please enter both todo and date");
+    return;
+  }
+
+  todoList.push({
+    item,
+    dueDate: date
+  });
+
+  todoInput.value = "";
+  todoDate.value = "";
+
+  renderTodos();
 }
 
-function displayItems() {
-  let containerElement = document.querySelector('.todo-container');
-  let newHtml = '';
-  for (let i = 0; i < todoList.length; i++) {
-    let {item, dueDate} = todoList[i];
-    newHtml += `
-      <span>${item}</span>
-      <span>${dueDate}</span>
-      <button class='btn-delete' onclick="todoList.splice(${i}, 1);
-      displayItems();">Delete</button>
-    `;
-  }
-  containerElement.innerHTML = newHtml;
+function deleteTodo(index) {
+  todoList.splice(index, 1);
+  renderTodos();
 }
+
+function renderTodos() {
+
+  todoContainer.innerHTML = "";
+
+  todoList.forEach((todo, index) => {
+
+    const todoElement = document.createElement("div");
+    todoElement.classList.add("todo-item");
+
+    todoElement.innerHTML = `
+      <span class="todo-text">${todo.item}</span>
+      <span class="todo-date">${todo.dueDate}</span>
+      <button class="btn-delete">Delete</button>
+    `;
+
+    const deleteBtn = todoElement.querySelector(".btn-delete");
+
+    deleteBtn.addEventListener("click", () => {
+      deleteTodo(index);
+    });
+
+    todoContainer.appendChild(todoElement);
+
+  });
+}
+
+renderTodos();
